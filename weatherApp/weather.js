d95e3f36af65a089661cb214d698b2e091cc7d74
YunesHama <65160180+YunesHama@users.noreply.github.com>
2020-07-01 17:22:46 +0200

    Add files via upload

---
+++ weatherApp/weather.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".top-banner form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+});
+
+const apiKey = "51b5ff7f7bd3956ac7b18ed4b4a468b0";
+const inputVal = input.value;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        // do stuff with the data
+    })
+    .catch(() => {
+        msg.textContent = "Please search for a valid city 😩";
+    });
+const { main, name, sys, weather } = data;
+const icon = `https://openweathermap.org/img/wn/${
+    weather[0]["icon"]
+}@2x.png`;
+
+const li = document.createElement("li");
+li.classList.add("city");
+const markup = `
+  <h2 class="city-name" data-name="${name},${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+  </h2>
+  <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+  </div>
+  <figure>
+    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+    <figcaption>${weather[0]["description"]}</figcaption>
+  </figure>
+`;
+li.innerHTML = markup;
+list.appendChild(li);
+msg.textContent = "";
+form.reset();
+input.focus();
